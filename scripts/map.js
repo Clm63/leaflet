c26a1f3356b928c64e36a333c05a25c0f263d55e
Clm63 <89849646+Clm63@users.noreply.github.com>
2021-09-01 17:38:07 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -127,7 +127,7 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-          .bindPopup("<b>" + point['Name'] + '</b><br>' +
+          .bindPopup("<b><strong>" + point['Name'] + '</strong></b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') + '<b>Description : </b>' + 
           point['Description']+ '</b><br>' + '<b>Location : </b>' + point['Location'] + '</b><br>' + 
                      '<b>Budget : </b>' + point['Budget'] + '</b><br>' + '<b>Link : </b>' + point['Link']);
