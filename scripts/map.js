9c8e4f430743d5ad0436132aa1b60aab7834a69a
Clm63 <89849646+Clm63@users.noreply.github.com>
2021-09-09 16:59:47 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -130,8 +130,7 @@ $(window).on('load', function() {
           .bindPopup("<strong>" + point['Name'] + '</strong><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') + '<b>Description : </b>' + 
           point['Description']+ '</b><br>' + '<b>Location : </b>' + point['Location'] + '</b><br>' + 
-                     '<b>Budget : </b>' + point['Budget'] + '</b><br>' + '<b>Année : </b>' + point['Year'] + '</b><br>' + '<b>Link : </b>' 
-                     + <a href="point['Link']">point['Link']</a> 
+                     '<b>Budget : </b>' + point['Budget'] + '</b><br>' + '<b>Année : </b>' + point['Year'] + '</b><br>' + '<b>Link : </b>' + point['Link']
                     + '</b><br>' + '<b>Ref : </b>' + point['Ref']);
 
         if (layers !== undefined && layers.length !== 1) {
